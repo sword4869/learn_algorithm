b785bf675cd3ccded6139b0c03c0ea6e70aec858
sword4869 <1084662708@qq.com>
2024-03-05 23:55:05 +0800

    12

--- src/main/java/com/itheima/algorithm/recursion/E02HanoiTower.java
+++ src/main/java/com/itheima/algorithm/recursion/E02HanoiTower.java
@@ -1,7 +1,5 @@
 package com.itheima.algorithm.recursion;
 
-import org.springframework.util.StopWatch;
-
 import java.util.LinkedList;
 
 /**
@@ -12,19 +10,14 @@ public class E02HanoiTower {
     static LinkedList<Integer> b = new LinkedList<>();
     static LinkedList<Integer> c = new LinkedList<>();
 
-    static void init(int n) {
-        for (int i = n; i >= 1; i--) {
-            a.addLast(i);
-        }
-    }
-
     /**
      * <h3>移动圆盘</h3>
      *
      * @param n 圆盘个数
-     * @param a 由
-     * @param b 借
-     * @param c 至
+     * @param a 源
+     * @param b 辅助
+     * @param c 目标
+     *  T(n) = 2T(n-1) + c,  O(2^64)
      */
     static void move(int n, LinkedList<Integer> a,
                      LinkedList<Integer> b,
@@ -34,23 +27,18 @@ public class E02HanoiTower {
         }
         move(n - 1, a, c, b);   // 把 n-1 个盘子由a,借c,移至b
         c.addLast(a.removeLast()); // 把最后的盘子由 a 移至 c
-//        print();
         move(n - 1, b, a, c);   // 把 n-1 个盘子由b,借a,移至c
     }
 
-    // T(n) = 2T(n-1) + c,  O(2^64)
-
-
     public static void main(String[] args) {
-        StopWatch sw = new StopWatch();
-        int n = 1;
-        init(n);
+        // n 个盘子
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            a.addLast(i);
+        }
         print();
-        sw.start("n=1");
         move(n, a, b, c);
-        sw.stop();
         print();
-        System.out.println(sw.prettyPrint());
     }
 
     private static void print() {
